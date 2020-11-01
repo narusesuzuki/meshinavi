2335f9983467b30722fef1a5d024249dee9f017c
“Naruse” <satoshisennsyu@gmail.com>
2020-11-01 16:52:04 +0900

    test

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'RestaurantController@index');
+
 Route::resource('restaurants', 'RestaurantController', ['only' => ['index', 'show']]);;
